f3446eae019e00799d517142f7ea3d02c9944f9a
GHOST <ghostdevbusiness@gmail.com>
2021-09-29 23:32:33 +0100

    feat: support all guildchannelcreateoptions

--- src/options/TicketSystem.ts
+++ src/options/TicketSystem.ts
@@ -1,9 +1,10 @@
+import type { GuildChannelCreateOptions } from 'discord.js';
 import Joi from 'joi';
 
-export interface TicketSystemOptions {
+export interface TicketSystemOptions extends GuildChannelCreateOptions {
     name: string;
 }
 
 export const schema = Joi.object<TicketSystemOptions>({
     name: Joi.string().min(1).max(32).default('{owner.user.username}-{id}'),
-});
+}).unknown();
