4ef3deab452e0bb1f349a1887bdf1c479a865bbf
GHOST <ghostdevbusiness@gmail.com>
2021-09-29 21:23:23 +0100

    fix: create client

--- dev/index.js
+++ dev/index.js
@@ -2,6 +2,12 @@ import { config } from 'dotenv';
 config({ path: 'dev/.env' });
 
 import { TicketSystem } from 'djs-ticketsystem';
-import { Intents } from 'discord.js';
+import { Intents, Client } from 'discord.js';
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.on('ready', () => console.log('Online'));
 
 client.login();
