6a21791289109031b14ef00683a461b0900db8ff
GHOST <ghostdevbusiness@gmail.com>
2021-09-29 22:00:15 +0100

    dev: create ticket

--- dev/index.js
+++ dev/index.js
@@ -8,6 +8,10 @@ const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
+const tickets = new TicketSystem(client);
+
+tickets.create('663140687591768074');
+
 client.on('ready', () => console.log('Online'));
 
 client.login();
