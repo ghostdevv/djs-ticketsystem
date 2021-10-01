7da1aec9ede90f98d95db183a7b815f8d9ee519f
GHOST <ghostdevbusiness@gmail.com>
2021-10-01 23:47:59 +0100

    fix: text tickets

--- src/TicketChannel/TicketChannel.ts
+++ src/TicketChannel/TicketChannel.ts
@@ -1,11 +1,10 @@
+import { Client, Guild, GuildMember, Snowflake, TextChannel } from 'discord.js';
 import type { RawGuildChannelData } from 'discord.js/typings/rawDataTypes';
-import type { Client, Guild, GuildMember, Snowflake } from 'discord.js';
 import { Routes } from 'discord-api-types/v9';
-import { GuildChannel } from 'discord.js';
 import pkg from '../../package.json';
 import axios from 'axios';
 
-export type TicketChannelResolvable = Snowflake | TicketChannel | GuildChannel;
+export type TicketChannelResolvable = Snowflake | TicketChannel;
 
 export interface TicketChannelDataOptions {
     owner: GuildMember;
@@ -18,7 +17,7 @@ export type TicketChannelFromData = TicketChannelDataOptions & {
 
 export type TicketChannelData = RawGuildChannelData & TicketChannelDataOptions;
 
-export class TicketChannel extends GuildChannel {
+export class TicketChannel extends TextChannel {
     public readonly owner: GuildMember;
 
     constructor(guild: Guild, data: TicketChannelData) {
