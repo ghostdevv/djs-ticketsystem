0ace1fa1e2a75041a5d5bda5966d51ba0cf2c84f
GHOST <ghostdevbusiness@gmail.com>
2021-10-01 21:58:43 +0100

    chore: update exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export { TicketSystem } from './TicketSystem';
 
 export type { CreateOptions } from './options/create';
 export type { TicketSystemOptions } from './options/TicketSystem';
+
+export * from './TicketChannel/TicketChannel.d';
+export { TicketChannelManager } from './TicketChannel/TicketChannelManager';
