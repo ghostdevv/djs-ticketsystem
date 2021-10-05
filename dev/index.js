c909870e79260409a5a27ed56c98e8c60ac87eb9
GHOST <ghostdevbusiness@gmail.com>
2021-10-05 11:24:08 +0100

    dev: clear method

--- dev/index.js
+++ dev/index.js
@@ -12,6 +12,18 @@ const tickets = new TicketSystem(client);
 
 client.on('ready', () => console.log('Online'));
 
+// const guild = await client.guilds.fetch('663140687591768074');
+
+// if (guild) {
+//     const c = await guild.channels.fetch();
+
+//     for (const [id, channel] of c)
+//         if (channel.name.startsWith('ghost')) {
+//             channel.delete();
+//             console.log('Deleted ' + channel.name);
+//         }
+// }
+
 const ticket = await tickets.create({
     guild: '663140687591768074',
     owner: '282839711834177537',
